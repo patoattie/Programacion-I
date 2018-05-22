52aa84e0c8066635357bf5c30314c40afb92221f
pepe <zz@zz.com>
2018-05-22 19:32:44 -0300

    Clase 2 de archivos

--- pattie/10_Archivos/main.c
+++ pattie/10_Archivos/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+mockaroo.com -> generador de archivos con datos aleatorios
+fwrite -> Funcion para escribir
+fread -> Funcion para leer
+wb -> modo de escritura binario
+rb -> modo de lectura binario
+fscanf -> permite leer en modo texto cadenas con separadores de campos "%[^,],%[^,],%[^\n]\n" (separador ,).
+atoi atof -> parsea cadena de char a int y float respectivamente
+*/
+
 int main()
 {
     /*
@@ -13,20 +23,27 @@ int main()
 
     archivo = fopen("fichero.txt", "w"); //abro el archivo de texto para escritura (w) Write para texto
 
-    fprintf(archivo, "Hola"); //escribo en el archivo (solo para archivos de texto)
-    fprintf(archivo, "%s", "\nHola con mascara"); //escribo en el archivo con máscara (solo para archivos de texto)
-
-    fclose(archivo); //cierro el archivo
-
-    archivo = fopen("fichero.txt", "r"); //abro el archivo de texto para lectura (r) Read para texto
-
-    while(!feof(archivo)) //Mientras no haya llegado al fin del archivo
+    if(archivo != NULL)
     {
-        fgets(bufferIn, 100, archivo); //leo el archivo (variable, cant de caracteres que leo, puntero a FILE)
-        puts(bufferIn); //muestro la variable donde guardo lo que lei del archivo
-    }
+        fprintf(archivo, "Hola"); //escribo en el archivo (solo para archivos de texto)
+        fprintf(archivo, "%s", "\nHola con mascara"); //escribo en el archivo con máscara (solo para archivos de texto)
 
-    //remove("fichero.txt"); //elimino el archivo del disco
+        fclose(archivo); //cierro el archivo
+
+        archivo = fopen("fichero.txt", "r"); //abro el archivo de texto para lectura (r) Read para texto
+
+        while(!feof(archivo)) //Mientras no haya llegado al fin del archivo
+        {
+            fgets(bufferIn, 100, archivo); //leo el archivo (variable, cant de caracteres que leo, puntero a FILE)
+            puts(bufferIn); //muestro la variable donde guardo lo que lei del archivo
+        }
+
+        //remove("fichero.txt"); //elimino el archivo del disco
+    }
+    else
+    {
+        printf("\nNo existe la ruta donde quiere guardar el archivo\n");
+    }
 
     return 0;
 }
