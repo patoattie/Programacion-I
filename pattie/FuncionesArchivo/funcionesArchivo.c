e12c7839de4bff9a40c209a4ff5b203aaca16965
pattiemecon <zz@zz.com>
2018-05-28 18:23:35 -0300

    Sigo con funciones para archivos

--- pattie/FuncionesArchivo/funcionesArchivo.c
+++ pattie/FuncionesArchivo/funcionesArchivo.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "funcionesArchivo.h"
 
+int abrirArchivo(FILE* punteroArchivo, const char* pathArchivo, const char* modoArchivo)
+{
+    int resultadoAperturaArchivo = 0;
+
+    punteroArchivo = fopen(pathArchivo, modoArchivo);
+    if(punteroArchivo != NULL)
+    {
+        resultadoAperturaArchivo = 1;
+    }
+
+    return resultadoAperturaArchivo;
+}
+
+int validarModoArchivo(const char* modoArchivo)
+{
+    int retorno = 0;
+
+    if(strcmp(modoArchivo, MODO_ESCRITURA_BINARIO) == 0
+       || strcmp(modoArchivo, MODO_ESCRITURA_TEXTO) == 0
+       || strcmp(modoArchivo, MODO_LECTURA_BINARIO) == 0
+       || strcmp(modoArchivo, MODO_LECTURA_TEXTO) == 0)
+    {
+        retorno = 1;
+    }
+
+    return retorno;
+}
--- pattie/FuncionesArchivo/funcionesArchivo.h
+++ pattie/FuncionesArchivo/funcionesArchivo.h
@@ -1,6 +1,12 @@
 #ifndef FUNCIONES_ARCHIVO_H_INCLUDED
 #define FUNCIONES_ARCHIVO_H_INCLUDED
 
-int abrirArchivo(FILE*, const char[], int);
+#define MODO_LECTURA_TEXTO "r"
+#define MODO_ESCRITURA_TEXTO "w"
+#define MODO_LECTURA_BINARIO "rb"
+#define MODO_ESCRITURA_BINARIO "wb"
+
+int abrirArchivo(FILE* punteroArchivo, const char* pathArchivo, const char* modoArchivo);
+int validarModoArchivo(const char* modoArchivo);
 
 #endif // FUNCIONES_ARCHIVO_H_INCLUDED
